499781e57eb18423c4e53dea5f6b2341ed87e70a
Magno Valdetaro <mvaldetaro@gmail.com>
2018-04-26 08:19:51 -0300

    Cria Fetch

---
+++ database.php
@@ -0,0 +1,3 @@
+<?php
+    $connection = new PDO('mysql:dbname=db_crud_ajax;host=localhost', 'root', '');
+?>
--- upload/database.php
+++
@@ -1,3 +0,0 @@
-<?php
-    $connection = new PDO('mysql:host=localhost,dbname=db_crud_ajax', '');
-?>
